28e0f56933a3365f063fb3511c25447b09ca3e96
Ujjawal Joshi <66362774+cyber-rush@users.noreply.github.com>
2023-09-08 09:35:21 +0000

    variables understanding

---
+++ Basics/variables.js
@@ -0,0 +1,7 @@
+const password = 156789;
+let city = "Lucknow";
+var state = "UP";
+country = "India";
+let newValue;
+//password = 67854; // const in js can't be initialised again
+console.table([password, city, state, country,newValue]);
